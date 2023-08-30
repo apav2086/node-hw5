266a7fbd11b39c83720b0b7f601ab27f519662b4
apav2086 <apav2086@gmail.com>
2023-08-30 07:28:23 -0400

    avatarUpload check

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.route("/login").post(login);
 router.route("/signup").post(signup);
 router.route("/logout").post(logout);
 router.route("/current").post(current);
-router.route('/upload').post(avatarUpload, handleAvatarUpload);
+router.route("/upload").post(avatarUpload, handleAvatarUpload);
 router.route("/avatars").patch(avatarUpdate);
 
 module.exports = router;
